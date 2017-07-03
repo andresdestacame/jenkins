48ce615daf77573f8c82ffcb0950f903d039f7ab
Andres Martinez <andresgmartinezb@gmail.com>
2017-07-03 12:12:40 -0400

    .

---
+++ basica/basica/basica.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import Levenshtein
+import fuzzy
+
+def basica(numero):
+    suma = 0
+    for x in range(0, numero):
+        suma += x
+    return suma
+
+valor = basica(5)
+
+print(valor)
