aefe3fc1a1f299191b7bc9d6ac2d636ee5ab19c5
Andres Martinez <andresgmartinezb@gmail.com>
2017-07-03 17:33:27 -0400

    .

---
+++ otra/otra/otra.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Comentario Andres Martinez
+import Levenshtein
+import fuzzy
+
+def otra(numero):
+    suma = 0
+    for x in range(0, numero):
+        suma += x
+    return suma
+
+valor = otra(5)
+
+print(valor)
